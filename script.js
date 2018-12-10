7064900e49a8a3ceff7ee171fd1f68aa01d7716d
Michael <michaelward900@gmail.com>
2018-12-10 18:40:00 -0500

    Alignment

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ $(document).ready(function () {
   var rain_slider = document.getElementById("rain_box");
 
   login();
-  build_homepage();
+  //build_homepage();
   //put in all airports
-  add_to_page(slider.value, snow_slider.checked, rain_slider.checked);
+  //add_to_page(slider.value, snow_slider.checked, rain_slider.checked);
 
 
   //getcities();
   //build_flight_interface('BOS');
   //alert("are you logged in?");
-  //build_flight_interface('CLT');
+  build_flight_interface('CLT');
 
 });
 
@@ -33,6 +33,13 @@ var aidtocity = function (aid) {
     }
   }
 };
+var aidtocode = function (aid) {
+  for (let i = 0; i < allairports.length; i++) {
+    if (allairports[i].id == aid) {
+      return allairports[i].code;
+    }
+  }
+};
 
 // AJAX GET request for all flight instances
 // Iterate through all instances and find instance with matching fid
@@ -40,9 +47,17 @@ var aidtocity = function (aid) {
 var iscancelled = function (fid) {
   for (let i = 0; i < instances.length; i++) {
     if (instances[i].flight_id == fid) {
-      console.log('current id is: ' + instances[i].flight_id);
-      console.log('given fid is: ' + fid);
-      console.log('flight cancelled?:' + instances[i].is_cancelled);
+      //console.log('current id is: ' + instances[i].flight_id);
+      //console.log('given fid is: ' + fid);
+      //console.log('flight cancelled?:' + instances[i].is_cancelled);
+      var boolenvalue = instances[i].is_cancelled;
+      console.log(boolenvalue)
+      // if (boolenvalue == "true") {
+      //   return "CANCELLED"
+      // }
+      // if (boolenvalue == "false") {
+      //   return "NO"
+      // }
       return instances[i].is_cancelled;
     }
   }
@@ -126,37 +141,47 @@ var build_flight_interface = function (acode) {
   let arrivals = correctflights[1];
 
 
-  body.append('<div class="flight departures">');
+  body.append('<div class="flight departures"></div>');
   for (let i = 0; i < departures.length; i++) {
+
     console.log('departure' + i + departures[i].arrival_id);
-    body.append(`
-        <div class="d" `+ i + ` >
-            <span class="time"> Departs at: ` + departures[i].departs_at.slice(11, 16) + ` </span>
-            <span class="destination"> Destination: ` + aidtocity(departures[i].arrival_id) + `</span>
-            <span class="flightnum"> Flight number: ` + departures[i].number + `  </span>
-            <span class="cancel"> Is cancelled? `+ iscancelled(departures[i].id) + `<button class="cancel" onclick="cancelflight( ` + departures[i].id + `)"> Cancel Flight </button>   </span>
-        </div>
-        `);
+    let departures_div = $('.departures')
+    let other_div = $('<div class="a"></div>')
+    other_div.append('<span class="time"> Departs at: ' + departures[i].departs_at.slice(11, 16) + ' </span>')
+    other_div.append('<span class="destination"> Destination: ' + aidtocode(departures[i].arrival_id) + '</span>')
+    other_div.append('<span class="flightnum"> Flight number: ' + departures[i].number + '  </span>')
+    other_div.append('<span class="cancel"> Is cancelled? ' + iscancelled(departures[i].id) + '<button class="cancel" onclick="cancelflight( ' + departures[i].id + ')"> Cancel Flight </button>   </span>')
+
+    departures_div.append(other_div);
+
   }
   // Separate departures and arrivals
-  body.append('</div>');
-  body.append('<br> <br>');
+  //body.append('</div>');
+  //body.append('<br> <br>');
 
 
   body.append('<div class="flight arrivals">');
   for (let i = 0; i < arrivals.length; i++) {
-    body.append(`
-         <div class="a` + i + `">
-            <span class="time"> Departs at: ` + arrivals[i].departs_at.slice(11, 16) + ` </span>
-            <span class="destination"> Destination: ` + aidtocity(arrivals[i].departure_id) + ` </span>
-            <span class="flightnum"> Flight number: ` + arrivals[i].number + `  </span>
-            <span class="cancel"> Is cancelled? `+ iscancelled(arrivals[i].id) + `<button class="cancel" onclick="cancelflight( ` + arrivals[i].id + `)"> Cancel Flight </button>   </span>
-         </div>
-        `);
+    //   body.append(`
+    //        <div class="a` + i + `">
+    //           <span class="time"> Arrives at: ` + arrivals[i].arrives_at.slice(11, 16) + ` </span>
+    //           <span class="destination"> From: ` + aidtocity(arrivals[i].departure_id) + ` </span>
+    //           <span class="flightnum"> Flight number: ` + arrivals[i].number + `  </span>
+    //           <span class="cancel"> Is cancelled? `+ iscancelled(arrivals[i].id) + `<button class="cancel" onclick="cancelflight( ` + arrivals[i].id + `)"> Cancel Flight </button>   </span>
+    //        </div>
+    //       `);
+    // }
+
+    // body.append('</div>');
+
+    let arrival_div = $('.arrivals')
+    let other_div = $('<div class="d"></div>')
+    other_div.append('<span class="time"> Arrives at: ' + arrivals[i].arrives_at.slice(11, 16) + ' </span>')
+    other_div.append('<span class="destination"> From: ' + aidtocode(arrivals[i].departure_id) + '</span>')
+    other_div.append('<span class="flightnum"> Flight number: ' + arrivals[i].number + '  </span>')
+    other_div.append('<span class="cancel"> Is cancelled? ' + iscancelled(arrivals[i].id) + '<button class="cancel" onclick="cancelflight( ' + arrivals[i].id + ')"> Cancel Flight </button>   </span>')
+    arrival_div.append(other_div);
   }
-
-  body.append('</div>');
-
 };
 
 var login = function () {
